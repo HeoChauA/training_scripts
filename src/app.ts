5fa112f04e6200908b02e0b59300ea645da5a4b5
Thien <dangduchoanthien0311@gmail.com>
2024-07-27 05:55:59 +0700

    baitap

--- src/app.ts
+++ src/app.ts
@@ -76,6 +76,8 @@ console.log(gcd(30,50));
 let power:any = (a:number, b:number) => {
     if (b === 0) return 1;
 
+    if (b < 0) return 1 / power(a, -b);
+
     return a * power(a, b-1);
 };
 
@@ -105,12 +107,12 @@ let prime:any = (a:number, b = 2) => {
 
     if (a < 2 || a % b === 0) return false;
 
-    if (a * a > b) return true;
+    if (a < b * b) return true;
     
     return prime(a, b + 1);
 };
 
-console.log(prime(2));
+console.log(prime(9));
 
 //Bai A13
 let countElement:any = (a:Array<any>, b:any) => {
@@ -191,3 +193,203 @@ console.log(mergeArrayNoDup(arr1, arr2, arr3));
 let pushArray = (...a:any) => ([...a]);
 
 console.log(pushArray(`a`, 45, `jdas`));
+
+//Bai A15
+let power2:any = (x:number, n:number) => {
+    if (n === 0) return 1;
+
+    if (n < 0) return 1 / power2(x, -n);
+
+    return x * power2(x, n-1);
+};
+
+console.log(power2(4, -3));
+
+//Bai A16
+let convert10to2:any = (a:number) => {
+    if (a >= 0 && a <= 1 ) return a;
+
+    return `${convert10to2(Math.floor(a/2))}${a%2}`;
+};
+
+console.log(convert10to2(7));
+
+//Bai A17
+let combination:any = (n:number, k:number) => {
+    if (k > n) return 0;
+
+    if (k === 0 || k === n) return 1;
+
+    return combination(n-1, k-1) + combination(n-1, k);
+
+    // Chung minh nCk = (n-1)C(k-1) + (n-1)Ck 
+    // https://www.toppr.com/ask/question/prove-that-n-1cr-n-1cr-1-ncr/
+};
+
+console.log(combination(8,4));
+
+//Bai A18
+let sumPrime:any = (n:number) => {
+    if (n < 2) return 0;
+
+    if (prime(n)) return n + sumPrime(n-1);   
+    
+    return sumPrime(n-1);
+};
+
+console.log(sumPrime(7));
+
+//Bao A19
+let permut:any = (a:string) => {
+    if (a.length < 2) return [a];    //Dieu kien de thoat de quy
+
+    let permutations = [];     //Array de chua cac hoan vi
+
+    for (let i = 0; i < a.length; i++) {         //Lap qua tung ky tu cua string a 
+        let char = a[i];         //Chon ky tu hien tai
+
+        if (a.indexOf(char) != i) {
+            continue;                   //Neu ky tu da duoc su dung thi bo qua no de khong bi trung lap
+        }
+
+        let remainingString = a.slice(0, i) + a.slice(i + 1, a.length);      //Chuoi con lai sau khi loai bo ky tu hien tai (char)
+
+        for (let string of permut(remainingString)) {           //De quy de tim cac hoan vi voi chuoi con lai
+            permutations.push(char + string);                   //Push ket hop char voi cac hoan vi cua remainingString vao array
+        }
+    }
+    return permutations;        //Tra ve Array chua cac hoan vi 
+};
+
+console.log(permut(`AABC`));
+
+//Bai A20
+let sumDimensionArr:any = (a:Array<number>) => {
+
+    let sum = 0;
+
+    for (let i = 0; i < a.length; i++) {        //Lap qua tung phan tu cua mang
+        if (Array.isArray(a[i])) {
+            sum += sumDimensionArr(a[i]);          //Neu phan tu thu i la mot mang con thi de quy phan tu do de tinh tong roi cong vao sum
+        }
+        else {
+            sum += a[i];                           //Con khong thi cong phan tu do vao sum
+        }
+    }
+
+    return sum;
+};
+
+let dimensionArr = [[1, 2, 3], 4, [5, 6, 7]];
+
+console.log(sumDimensionArr(dimensionArr));
+
+//Bai A21
+const data = {
+    user: {
+        name: 'John',
+        address: {
+            city: 'New York',
+            zip: '10001'
+        }
+    },
+    office: {
+        address: {
+            city: 'San Francisco',
+            zip: '94101'
+        }
+    },
+};
+
+let printData:any = (a:any, b:any) => {
+    for (let key in a) {
+        if (key === b) {
+            return a[key];
+        }
+
+        if (a[key] && typeof a[key] === `object`) {
+            if (printData(a[key], b) !== undefined) {
+                return printData(a[key], b);
+            }
+        }
+    }
+
+    return `Khong tim thay khoa`;
+};
+
+console.log(printData(data, 'zip'));
+
+// Bai A22
+const nestedObject = {
+    a: 1,
+    b: { 
+        c: 2,
+        d: {
+            e: 3,
+        },
+    },
+    f: {
+        g: 4,
+    },
+};
+
+let countKey:any = (a:any) => {
+    let count = 0;
+    for (let key in a) {
+        if (a[key]) {
+            count += 1;
+        }
+
+        if (a[key] && typeof a[key] === `object`) {
+            count += countKey(a[key]);
+        }
+    }
+    return count;
+};
+
+console.log(countKey(nestedObject));
+
+//Bai A23
+let printValue:any = (a:any, b:any) => {
+    let info = [];
+    for (let key in a) {
+        if (key === b) {
+            info.push(a[key]);
+        }
+
+        if (a[key] && typeof a[key] === `object`) {
+            info = info.concat(printValue(a[key], b))
+        }
+    }
+
+    return info;
+};
+
+console.log(printValue(data, 'city'));
+
+//Bai C1
+let hello = `Hello, World!`;
+
+setTimeout(() => console.log(hello), 2000);
+
+//Bai C2
+let countDown = (a:number) => {
+    for (let i = a; i > 0; i--) {
+        setTimeout(() => console.log(i), (a-i)*1000);
+    }
+
+    setTimeout(() => console.log(`Happy New Year!`), a*1000);
+};
+
+countDown(5);
+
+//Bai C3
+let prints = () => {
+    let timeoutID = setTimeout(() => console.log(`Chi hien thi trong 2 giay`), 5000);
+    setTimeout(() => {
+        clearTimeout(timeoutID);
+        console.log(`Da xoa`);
+    }, 2000);
+};
+
+prints();
